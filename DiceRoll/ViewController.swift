55fc7cda0b32181aa12e50207d0c863a56efc409
Nila Dharmaraj <nila@dharmaraj.com>
2019-07-17 14:51:52 -0700

    dice roll with bears, works, constraints X

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
+    var dice = ["Dice Green 1", "Dice Green 2", "Dice Green 3", "Dice Green 4", "Dice Gree 5", "Dice Green 6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        let rndNum = Int.random(in: 0...5)
+        let rndNum2 = Int.random(in: 0...5)
+        dice1.image = UIImage(named :dice[rndNum])
+        dice2.image = UIImage(named :dice[rndNum2])
+    }
+    
 }
 
